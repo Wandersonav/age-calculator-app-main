d54bf6ecf1610aaa5984d2ac9c22d562e1b370cc
Wanderson Alves Vieira <wanderson.vieira@goias.gov.br>
2023-05-18 17:43:42 -0300

    atualização

--- main.js
+++ main.js
@@ -1,30 +1,38 @@
 
-let enviar = document.getElementById('enviar');
+// let enviar = document.getElementById('enviar');
 
-
-
-let dia = document.getElementById('day');
-let mes = document.getElementById('month');
-let ano = document.getElementById('year');
-
+// let dia = document.getElementById('day');
+// let mes = document.getElementById('month');
 
 
 
 
-console.log(dia.value)
-//dia.addEventListener('keyup', envio)
-//mes.addEventListener('keyup', envio)
-//ano.addEventListener('keyup', envio)
-enviar.addEventListener('click',envio)
+ enviar.addEventListener('click',envio)
 
 function envio(e){
-  e.preventDefault();
-  console.log(dia.value +'/'+ mes.value + '/' + ano.value) 
-  
+   e.preventDefault();
+   let ano_atual = new Date().getFullYear();
+   let ano_informado = document.getElementById('year');
+
+let ano_informado_string = JSON.stringify(ano_informado.value);
+let ano_informado_numero = parseInt(ano_informado_string);
+
+   console.log(ano_atual - ano_informado_string);
+    console.log(ano_informado_string)
+    console.log(typeof ano_atual)
+    console.log(ano_informado_numero.value)
+
+
+//let data_aniversario = dia.value +'/'+ mes.value + '/' + ano.value
+
+
 
 }
 
 
-//console.log(juntos)
+
 //console.log(parseInt (juntos.value))
 
+// var ano_atual = new Date().getFullYear();
+// var ano_informado = 1970;
+// console.log(ano_atual - ano_informado);
